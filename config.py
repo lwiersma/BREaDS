16d8c113d0539f53b6f618a202b0b909e24c8b36
L. Wiersma <larswiersma@icloud.com>
2024-08-13 11:41:24 +0200

    Use of dictionaries instead of separate config.py file.

--- config.py
+++
@@ -1,8 +0,0 @@
-'''
-This file contains the configurations of the energy system.
-'''
-
-charge_rate = 800.0 #[kW]
-discharge_rate = 800.0 #[kW] 
-battery_capacity = 22000.0 #[kWh]
-initial_state = 10.0 #[kWh]
---
+++ dictionaries/battery_config.py
@@ -0,0 +1,9 @@
+'''
+This dictionary contains the configurations of the battery.
+'''
+battery_config = {
+    'charge_rate': 800.0        # kW
+    'discharge_rate': 800.0     # kW
+    'battery_capacity': 2500    # kWh
+    'initial_state': 450        # kWh
+}
